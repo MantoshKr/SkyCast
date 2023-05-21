f9be14ab2a1771d4030941f59933efb681bdd252
mantoshdev <123961664+mantoshdev2@users.noreply.github.com>
2023-05-21 19:23:29 +0530

    weather-app-complete

--- src/components/forecast/forecast.js
+++ src/components/forecast/forecast.js
@@ -1,27 +1,67 @@
 import { Accordion, AccordionItem, AccordionItemButton, AccordionItemHeading, AccordionItemPanel } from "react-accessible-accordion";
 import "./forecast.css"
 
-
-
-
+const WEEK_DAYS = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
 
 const Forecast = ({ data }) => {
+
+    const dayInAWeek = new Date().getDay();
+
+    const forecastDays = WEEK_DAYS.slice(dayInAWeek, WEEK_DAYS.length).concat(WEEK_DAYS.slice(0, dayInAWeek));
+
+
+
     return (
-        
+
+
         <div>
 
             <label className="title">Daily</label>
             <Accordion allowZeroExpanded>
-           {data.list.splice(0, 7).map((item, idx) => (
-                <AccordionItem key={idx}>
-                    <AccordionItemHeading>
-                        <AccordionItemButton>
-                            hello
-                        </AccordionItemButton>
-                    </AccordionItemHeading>
-                    <AccordionItemPanel></AccordionItemPanel>
-                </AccordionItem>
+                {data.list.splice(0, 7).map((item, idx) => (
+                    <AccordionItem key={idx}>
+                        <AccordionItemHeading>
+                            <AccordionItemButton>
+                                <div className="daily-item">
+                                    <img alt="weather" className="icon-small" src={`icons/${item.weather[0].icon}.png`} />
+                                    <label className="day">{forecastDays[idx]}</label>
+                                    <label className="description">{item.weather[0].description}</label>
+                                    <label className="min-max-temp">{Math.round(item.main.temp_min)}°C /{Math.round(item.main.temp_max)}°C</label>
+                                </div>
+                            </AccordionItemButton>
+                        </AccordionItemHeading>
+                        <AccordionItemPanel>
+                            <div className="daily-details-grid">
+                                <div className="daily-details-grid-item">
+                                    <label className="pressure">Pressure </label>
+                                    <label className="pressure">{item.main.pressure}hpa</label>
+                                </div>
+                                <div className="daily-details-grid-item">
+                                    <label className="humidity">humidity </label>
+                                    <label className="humidilty">{item.main.humidity}%</label>
+                                </div>
+                                <div className="daily-details-grid-item">
+                                    <label className="clouds">clouds </label>
+                                    <label className="clouds">{item.clouds.all}%</label>
+                                </div>
+                                <div className="daily-details-grid-item">
+                                    <label className="wind speed">wind speed </label>
+                                    <label className="wind speed">{item.wind.speed}m/s</label>
+                                </div>
+                                <div className="daily-details-grid-item">
+                                    <label className="sea level">sea level </label>
+                                    <label className="sea level">{item.main.sea_level}</label>
+                                </div>
+                                <div className="daily-details-grid-item">
+                                    <label className="feels like">Feels like </label>
+                                    <label className="feels like">{item.main.feels_like}°C</label>
+                                </div>
+                            </div>
+
+
+                        </AccordionItemPanel>
+                    </AccordionItem>
                 ))}
             </Accordion>
         </div>
