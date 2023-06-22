d6bc901f86d39539e873b614bb4433440b05c7af
mantoshdev <123961664+mantoshdev2@users.noreply.github.com>
2023-06-22 17:15:02 +0530

    created .env file

--- src/api.js
+++ src/api.js
@@ -13,5 +13,5 @@ export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
 
 
-export const WEATHER_API_KEY = "7191f7d1b8ae322fca37b55915a85a10";
+export const WEATHER_API_KEY=process.env.REACT_APP_WEATHER_API_KEY;
 
